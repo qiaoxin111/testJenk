aa845d5591609da8ef83a75289bc2feac94ab7cf
qiaoxin <793021821@qq.com>
2021-08-04 15:26:37 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
   haskhdfalksjfka
   <div>这是我的测试项目，测试jenkins</div>
   <div>这是我的测试项目，测试jenkins</div>
+  <div>这是我的测试项目，测试jenkins</div>
+  <div>这是我的测试项目，测试jenkins</div>
+  <div>这是我的测试项目，测试jenkins</div>
+  <div>这是我的测试项目，测试jenkins</div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
