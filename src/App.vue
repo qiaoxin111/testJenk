fabb2610f74e0bfcab8874712890eea00a9017ff
wanwgqx <793021821@qq.com>
2024-12-29 16:56:06 +0800

    feat: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-    
+    <img alt="Vue logo" src="./assets/logo.png" />
+
     <div>测试CI</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
     <div>hahahahahhhahhahaahh</div>
     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
-     <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
+    <div>测试自动化部署啦啦啦啦啦</div>
 
-  
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
